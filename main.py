e946f212f45f2397985a43d14331525aeb2a8a44
Nooble <96099842+NooblePrime@users.noreply.github.com>
2025-01-17 16:38:41 -0600

    updated main.py
    Switched out some string checks for regex and added an exception for when threads are locked.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pprint import pprint
 from checks import checkIfVisited, logVisit
 from redditFunctions import attemptComment, markbot, unmarkbot
 from vars import reddit, bot_statement
+import re
 
 def mentionStream():
     retry = True
@@ -23,10 +24,10 @@ def mentionStream():
                     item.mark_read()
                 if item.new and ((info['type'] == 'username_mention') or (info['type'] == 'comment_reply' and 'u/bot-sleuth-bot' in body)):
                     if 'repost' in body:
-                        if 'filter: subreddit' in body or 'filter:subreddit' in body:
+                        if re.search(r"filter:[\s]{0,1}subreddit", body) is not None:
                             attemptComment(item.parent().author, item, True, constraint = 'subreddit')
-                        elif 'filter: reddit' in body or 'filter:reddit' in body:
-                            attemptComment(item.parent().author, item, True, constraint = 'subreddit')
+                        elif re.search(r"filter:[\s]{0,1}reddit", body) is not None:
+                            attemptComment(item.parent().author, item, True, constraint = 'reddit')
                         else:
                             attemptComment(item.parent().author, item, True)
                         print('Replied to a post!')
@@ -57,7 +58,7 @@ def mentionStream():
                         print('Replied to a post!')
                         item.mark_read()
             retry = False
-        except ServerError:
+        except ServerError or TooManyRequests:
             print("Server error. Retrying...")
             sleep(5)
             retry = True
@@ -79,10 +80,10 @@ def mentionStream():
                     item.mark_read()
                 elif item.new and ((info['type'] == 'username_mention') or (info['type'] == 'comment_reply' and 'u/bot-sleuth-bot' in body)):
                     if 'repost' in body:
-                        if 'filter: subreddit' in body or 'filter: subreddit' in body:
+                        if re.search(r"filter:[\s]{0,1}subreddit", body) is not None:
                             attemptComment(item.parent().author, item, True, constraint = 'subreddit')
-                        elif 'filter: reddit' in body or 'filter:reddit' in body:
-                            attemptComment(item.parent().author, item, True, constraint = 'subreddit')
+                        elif re.search(r"filter:[\s]{0,1}reddit", body) is not None:
+                            attemptComment(item.parent().author, item, True, constraint = 'reddit')
                         else:
                             attemptComment(item.parent().author, item, True)
                         print('Replied to a post!')
@@ -113,7 +114,7 @@ def mentionStream():
                         print('Replied to a post!')
                         item.mark_read()
             retry = False
-        except ServerError:
+        except ServerError or TooManyRequests:
             print("Server error. Retrying...")
             sleep(5)
             retry = True
