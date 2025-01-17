a13db57cedbb573820e67a181835d7818b43a215
Nooble <96099842+NooblePrime@users.noreply.github.com>
2025-01-16 21:47:27 -0600

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,7 @@
+import praw
+reddit = praw.Reddit(
+    #REDACTED CODE
+)
+new_line = "\n\n"
+bot_statement = "\n\n^(I am a bot. This action was performed automatically. I am also in early development, so my answers might not always be perfect.)"
+trusted_users = #REDACTED
