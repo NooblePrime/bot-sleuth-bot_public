9b59bd0cdb0f849429413f726fa19c1f58ffa4ef
Nooble <96099842+NooblePrime@users.noreply.github.com>
2025-02-03 21:18:01 -0600

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,5 +3,7 @@ reddit = praw.Reddit(
     #REDACTED CODE
 )
 new_line = "\n\n"
-bot_statement = "\n\n^(I am a bot. This action was performed automatically. I am also in early development, so my answers might not always be perfect.)"
+bot_statement = "\n\n^(I am a bot. This action was performed automatically. Check my profile for more information.)"
 trusted_users = #REDACTED
+def banStatement(subreddit_name: str, comment_link: str, comment_string: str):
+    return f"Hello. It appears that I have been banned from r/{subreddit_name.replace('_', '\\_')} and am unable to reply to [this comment]({comment_link}), so I have privately messaged you to display the results instead. The constructed reply is as follows:\n\n" + comment_string + bot_statement
