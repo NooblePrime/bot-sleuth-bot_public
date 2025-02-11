854be2a5d3114df0ef9a8607118dc9c02cc52fbd
Nooble <96099842+NooblePrime@users.noreply.github.com>
2025-02-10 22:35:24 -0600

    Add files via upload

--- imageHandling.py
+++ imageHandling.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def imageSearch(image_url: str):
     output = []
     session = requests.Session()
     session.headers.update({'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'})
-    upload_response = session.get('https://lens.google.com/uploadbyurl', params={'url': image_url.replace("preview", "i", 1)}, timeout=5)
+    params = {'url': image_url.replace("preview", "i", 1)}
+    upload_response = session.get('https://lens.google.com/uploadbyurl', params=params, timeout=5)
     soup = BeautifulSoup(upload_response.text, features="html.parser")
     exact = next((str(links.get('href')) for links in soup.find_all('a') if 'udm=48' in str(links.get('href'))), '')
     response = session.get(upload_response.url + exact, timeout=5)
     soup2 = BeautifulSoup(response.text, features="html.parser")
-    output = [str(links.get('href')) for links in soup2.find_all('a') if links.get('href') and '/search?' not in links.get('href') and 'google.com/' not in links.get('href')]
+    output = [str(links.get('href')) for links in soup2.find_all('a') if links.get('href') and not re.search(r'/search\?|google\.com/|https://www\.google\.ca/intl/|https://about\.google|https://www\.google\.com/intl/|https://www\.google\.com/preferences', links.get('href'))]
     return output
 
 def sortMedia(gallery_data):
